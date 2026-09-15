65a6fc65a416541e7a379af87db56311eab7eb4e
abdlrahman-l <abdulrahmaninformatics@gmail.com>
2026-09-15 19:44:03 +0700

    add comment

--- src/db/request.ts
+++ src/db/request.ts
@@ -1,3 +1,7 @@
+/**
+ * Converts an IndexedDB request from its event-based API into a Promise,
+ * allowing it to be used with async/await.
+ */
 export function requestToPromise<T>(request: IDBRequest<T>): Promise<T> {
   return new Promise((resolve, reject) => {
     request.onsuccess = () => resolve(request.result)
